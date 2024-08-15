d3cfbf2cef56d04d00c83bc4b1b6b7477fc69130
qkenn <github.com.sampling990@passinbox.com>
2024-08-15 10:05:48 +0530

    feat: add basic layout

---
+++ src/components/CV.jsx
@@ -0,0 +1,7 @@
+export function CV() {
+  return (
+    <>
+      <div className="cv">cv</div>
+    </>
+  );
+}
---
+++ src/components/CVUpdater.jsx
@@ -0,0 +1,7 @@
+export function CVUpdater() {
+  return (
+    <>
+      <div className="updater">updater</div>
+    </>
+  );
+}
